f8eba6da043741d8bd0a66131dd39ed7302f8ff7
Markus Bernhardt <markus.bernhardt@me.com>
2025-02-20 23:05:12 +0100

    Small fixes

--- src/app/services/gateway.service.ts
+++ src/app/services/gateway.service.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GatewayService {
-    selectedGatewayUrl: string = 'localhost';
+    selectedGatewayUrl: string = 'http://localhost:8080';
 }
